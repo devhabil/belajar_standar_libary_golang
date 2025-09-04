5eb94c0d0d9ff650bcb0822712d17b87d352c103
Muhammad Habil Arifin <mr.habilhabil1405@gmail.com>
2025-09-04 21:39:47 +0700

    fmt libary

---
+++ fmt.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	firtName := "habil"
+	lastName := "arifin"
+
+	fmt.Println("Hello `", firtName, lastName, "`")
+	fmt.Printf("Hello `%s %s`\n", firtName, lastName)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module belajar_standar_libary_golang
+
+go 1.21.1
