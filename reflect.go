92a0e22c6b31ecea62ed713e3e021d85d9d447fc
Muhammad Habil Arifin <mr.habilhabil1405@gmail.com>
2025-09-28 21:58:59 +0700

    reflect

---
+++ reflect.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Sample struct {
+	Name string
+}
+type Person struct {
+	Name, Address, Email string
+}
+
+func readField(value any) {
+	var valueType reflect.Type = reflect.TypeOf(value)
+	fmt.Println("Type Name", valueType.Name())
+	for i := 0; i < valueType.NumField(); i++ {
+		var valueField reflect.StructField = valueType.Field(i)
+		fmt.Println(valueField.Name, "with type", valueField.Type)
+	}
+}
+
+func main() {
+	readField(Sample{"Habil"})
+	readField(Person{"Habil", "", ""})
+}
